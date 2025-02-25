b8e2968b51079e32d7923b6dfcb7fcda443cb823
andherewemet <escapeyourself58@gmail.com>
2025-02-25 18:50:22 +0530

    Add files via upload

---
+++ sites/1oTmONd0gjRiAEfXiqQ63x/chunk-JRZKJX2J.mjs
@@ -0,0 +1,18 @@
+function a(t,e){return{customHTMLBodyStart:`<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5TCNZFX4" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`,customHTMLHeadEnd:`<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5TCNZFX4');<\/script>
+<!-- End Google Tag Manager -->`,customHTMLHeadStart:`<!-- Google tag (gtag.js) -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-BMTKBNP7P9"><\/script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BMTKBNP7P9');
+<\/script>`,description:"Join andwemet a community designed for singles 28, 29 in their 30s, 40s, 50s. Search for matches, share your story, find your special someone one, all while maintaining privacy. ",favicon:"https://framerusercontent.com/assets/XXRuFIlBfokYiTWvHntZaYCp4.svg",robots:"max-image-preview:large",title:"andwemet"}}export{a};
+//# sourceMappingURL=chunk-JRZKJX2J.mjs.map
